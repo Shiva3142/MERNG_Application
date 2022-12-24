e46c160fbd4bee6b7a02e174ed0d6b8c08093ebc
TechForEverybody <Pankajsinghat95@gmail.com>
2022-12-24 14:27:09 +0530

    Updation

--- backend/utils/auth-check.js
+++ backend/utils/auth-check.js
@@ -1,29 +1,28 @@
-const {
-    AuthenticationError
-} = require('apollo-server-express')
+const { AuthenticationError } = require("apollo-server-express");
 
-const jwt = require('jsonwebtoken');
-const {SECRET_KEY}=require('../config.js')
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config.js");
 
 async function checkAuth(context) {
     let authHeader = context.req.headers.authorization;
     if (authHeader) {
         try {
             let user = jwt.verify(authHeader, SECRET_KEY);
-            return user
+            return user;
         } catch (error) {
             console.log(error);
-            throw new AuthenticationError('Invalid/Expired Tocken',{errors:{
-                token:"Please Login First"
-            }
-            })
+            throw new AuthenticationError("Invalid/Expired Tocken", {
+                errors: {
+                    token: "Please Login First",
+                },
+            });
         }
     }
-    throw new AuthenticationError('Authentication Header Must be provided',{
-        errors:{
-            header:"Please Login First"
-        }
-    })
+    throw new AuthenticationError("Authentication Header Must be provided", {
+        errors: {
+            header: "Please Login First",
+        },
+    });
 }
 
-module.exports={checkAuth}
+module.exports = { checkAuth };
